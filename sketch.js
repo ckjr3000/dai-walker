662c185100ba5d5e87d7e6e8075dba2d9d6e203d
Charlotte Roe <charlotte.kj.roe@gmail.com>
2023-01-08 13:21:21 +0000

    load in audio, map rect size to amplitude

--- sketch.js
+++ sketch.js
@@ -1,21 +1,43 @@
-let pos;
-let pallette = [];
+let pos, track, amp;
+
+function preload() {
+  track = loadSound("./placeholder.mp3");
+}
 
 function setup() {
-  createCanvas(2000, 1000);
+  let canvas = createCanvas(2000, 1000);
   background(0);
+
   pos = createVector(width / 2, height / 2);
+
+  canvas.mouseClicked(togglePlay);
+  amp = new p5.Amplitude();
 }
 
 function draw() {
-  fill(255);
+  let level = amp.getLevel();
+
+  let color = 255;
+  let size = map(level, 0, 1, 0, 200);
+
+  fill(color);
   noStroke();
-  rect(pos.x, pos.y, 5, 5);
+  rect(pos.x, pos.y, size, size);
 
   pos.x += random(-5, 5);
   pos.y += random(-5, 5);
 
   if ((pos.x > width) | (pos.x < 0) | (pos.y > height) | (pos.y < 0)) {
     pos = createVector(width / 2, height / 2);
+  }
+}
+
+function togglePlay() {
+  if (track.isPlaying()) {
+    track.pause();
+  } else {
+    track.loop();
+    amp = new p5.Amplitude();
+    amp.setInput(track);
   }
 }
